71fe1a3cc001476f1ae533739670ac2b1bf4a676
Yu Hongchao <2871314919@qq.com>
2018-06-27 10:13:39 +0800

    课表查询

--- src/main/resources/static/js/script/calendar.js
+++ src/main/resources/static/js/script/calendar.js
@@ -10,13 +10,31 @@ require(['jquery','moment','jquery_ui','zhcn'],function ($,moment,b,c) {
                     center: '',
                     right: ''
                 },
+                height : 600,
+                themeSystem: 'bootstrap3',
                 defaultView: 'agendaWeek',
+                slotMinutes : 120,
+                firstDay:1,
                 events : [{
                     title: "Event 1",
-                    start : moment().startOf("week").add(1, 'days'),
+                    start : moment().startOf("week").add({
+                        'days':1,
+                        'hours' : 8
+                    }),
                     end : moment().startOf('week').add({
                         'days':1,
-                        'hours' : 3
+                        'hours' : 10
+                    }),
+                    color: "blue",
+                },{
+                    title: "Event 2",
+                    start : moment().startOf("week").add({
+                        'days':0,
+                        'hours' : 10
+                    }),
+                    end : moment().startOf('week').add({
+                        'days':0,
+                        'hours' : 12
                     }),
                     color: "blue",
                 }]
