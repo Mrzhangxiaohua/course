7fc48b50b920e1471b8ecdbcde3c9d5e45da3f40
Yu Hongchao <2871314919@qq.com>
2018-06-29 09:49:02 +0800

    老师表单

--- src/main/java/com/spc/service/student/impl/StudentServiceImpl.java
+++ src/main/java/com/spc/service/student/impl/StudentServiceImpl.java
@@ -19,13 +19,15 @@ public class StudentServiceImpl  implements StudentService {
         String temp[][] = new String[5][7];
         for (HashMap<String,String> li : lis){
             String date = li.get("classDateDescription");
+            String classPlace = li.get("classPlace");
+            String teaName = li.get("teaName");
             String[] ints = date.split(":");
             Integer r = ints[0].toCharArray()[0]- '0';
             Integer l = ints[1].toCharArray()[0]- '0';
             System.out.println(r);
             System.out.println(l);
             System.out.println(li.get("className"));
-            temp[r-1][l-1] =  li.get("className");
+            temp[r-1][l-1] =  li.get("className") + ','+classPlace + ',' + teaName;
         }
         return temp;
     }
--- src/main/resources/static/js/script/calendar.js
+++ src/main/resources/static/js/script/calendar.js
@@ -5,15 +5,29 @@ require(['jquery'], function ($) {
         $.get("/data/Student/select?stuId=2018000001",function(data){
 
             var data = JSON.parse(data);
-            console.log(data);
+            // console.log(data);
             var elem = $("#tb");
             for(var i = 0;i<data.length;i++){
 
-                var strs = "<tr> <td>"+((i+1)*2-1)+ "-"+ (i+1)*2 +"</td>";
+                var strs = "<tr >"
+                if(i==0){
+                    strs += "<td rowspan='2' style='vertical-align:middle ' >上午</td>"
+                }
+                else if(i==2){
+                    strs += "<td rowspan=\"2\"  style='vertical-align:middle '>下午</td>"
+                }
+                else if(i==4){
+                    strs += "<td rowspan=\"1\" >晚上</td>"
+                }
+
+                strs += "<td>"+((i+1)*2-1)+ "-"+ (i+1)*2 +"</td>";
 
                 for(var j = 0;j<data[i].length;j++){
                     if(data[i][j]!=null){
-                        strs += "<td>" + data[i][j]+"</td>";
+                        var v = data[i][j].split(",");
+                        var s ="<pre style='background-color: #92f4ee'>" + "课程:"+ v[0] + "</pre>" +"<pre style='background-color: #ffcef9'>" +"教室:"+ v[1] +"</pre>" +"<pre style='background-color: #e0f977'>"+"老师:"+v[2] +"</pre>" ;
+                        console.log(s)
+                        strs += "<td>" + s +"</td>";
                     }else{
                         strs += "<td></td>";
                     }
