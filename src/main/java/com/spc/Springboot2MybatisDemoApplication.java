4274e405c94418661862864abba8fb27e8dde4ee
Yu Hongchao <2871314919@qq.com>
2018-07-20 21:04:31 +0800

    初步版本

--- src/main/java/com/spc/Springboot2MybatisDemoApplication.java
+++ src/main/java/com/spc/Springboot2MybatisDemoApplication.java
@@ -1,5 +1,6 @@
 package com.spc;
 
+//import net.unicon.cas.client.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 @SpringBootApplication
+//@EnableCasClient
 public class Springboot2MybatisDemoApplication extends SpringBootServletInitializer {
 
     @Override
--- src/main/java/com/spc/controller/MainController.java
+++ src/main/java/com/spc/controller/MainController.java
@@ -2,11 +2,14 @@ package com.spc.controller;
 
 
 import com.spc.util.RequestPayload;
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.util.HttpServletRequestWrapperFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -48,18 +51,9 @@ public class MainController {
     }
 
 
-    @RequestMapping("/login")
-    public String login(){
-        System.out.println("login here");
-        return "login";
-    }
 
     @RequestMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (auth != null) {
-//            new SecurityContextLogoutHandler().logout(request, response, auth);
-//        }
         System.out.println("run login");
         return "login";
     }
