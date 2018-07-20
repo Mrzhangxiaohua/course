6bf16554a12ce410dc76c8ac1e84e8d64f8b62e6
Yu Hongchao <2871314919@qq.com>
2018-07-20 21:05:03 +0800

    初步版本

---
+++ src/main/java/com/spc/dao/StudentApplicationDao.java
@@ -0,0 +1,8 @@
+package com.spc.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface StudentApplicationDao {
+
+    int add(@Param("stuId")Integer stuId,@Param("classId")Integer classId,@Param("states")Integer  states);
+}
