3bc0a6e1da8d0043c1c6497205516af4efc983e6
Mrzhangxiaohua <724183028@qq.com>
2019-04-15 18:20:06 +0800

    补充丢失代码

--- src/sql/ViewSql/course_Information.sql
+++ src/sql/ViewSql/course_Information.sql
@@ -1,17 +1,18 @@
-create view t_kc_byhx as
-       select a.classEncode  as KCBH,
-              a.className    as KCMC,
-              b.courseNameEN as KCYWMC,
-              c.departCode   as YXSH,
-              null           as KCXF,
-              a.classTime    as KCZXS,
-              a.teaId        as JSBH,
-              null           as KKJJ,
-              a.classId      as ID,
-              null           as XDND
-       from Course a
-              left join
-              mid as b
-              on a.className = b.courseNameCHS
-              left join Department as c on c.departId = a.departId;
+create
+  view t_kc_byhx as
+select a.classEncode  as KCBH, -- 课程编号
+       a.className    as KCMC,  -- 课程名称
+       b.courseNameEN as KCYWMC,-- 课程英文名称
+       c.departCode   as YXSH,-- 院系
+       null           as KCXF,-- 课程学分
+       a.classTime    as KCZXS,-- 课程学时
+       a.teaId        as JSBH,-- 教师编号
+       null           as KKJJ,-- 开课季节
+       a.classId      as ID,-- ID
+       null           as XDND  -- 学年年度
+from Course a
+       left join
+       mid as b
+       on a.className = b.courseNameCHS
+       left join Department as c on c.departId = a.departId;
 
