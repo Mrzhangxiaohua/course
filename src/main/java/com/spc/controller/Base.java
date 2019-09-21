9985a4a6b60735266a9e31c32446d2e7e87f3e1b
yl2004502 <yl2004502@qq.com>
2019-09-21 16:56:33 +0800

    调整学年学期

--- src/main/java/com/spc/controller/Base.java
+++ src/main/java/com/spc/controller/Base.java
@@ -18,11 +18,10 @@ import java.net.URLEncoder;
 public class Base {
     //static：保证全局唯一性，静态变量属于类
     //public：确保任意包里的子类都可以调用这些属性。
-
-    protected static String CURRENTSEMESTER = "2018-2019春季";
+    protected static String CURRENTSEMESTER = "2019-2020秋季";
 
     // 当时秋季学期的时候，请把这个字段设置为null
-    protected static String PREVIOUSSEMESTER = "2018-2019秋季";
+    protected static String PREVIOUSSEMESTER = null;
 
     public final Logger logger = LoggerFactory.getLogger(Base.class);
 
